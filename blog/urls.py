230292a34dff18651e71c5574293c7f78e5b5399
Easy <easy-quest@mail.ru>
2021-08-21 11:43:53 +0000

    first

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 # blog/urls.py
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
 ]
-
 """[summary]
     импортируем views 
 Мы импортируем views (которые скоро создадим) в верхней части. Пустая строка ''
